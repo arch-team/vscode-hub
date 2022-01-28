19cd263824c1ed6b2bb33a448ef3792076bc5c5a
sjh <277479420@qq.com>
2022-01-28 21:18:58 +0800

    iiiiii

---
+++ learn-debug/app.js
@@ -0,0 +1,27 @@
+
+class Person {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+
+    }
+
+    getName() {
+        const name = this.lastName + this.firstName;
+        return name;
+    }
+
+    say() {
+        const name = this.getName();
+        const text = name + ":" + this.age;
+        return text; ßß
+    }
+
+}
+
+const person = new Person("三", "张", 20);
+
+const text = person.say();
+
+console.log(text);
